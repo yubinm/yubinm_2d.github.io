947d05e612a39d26a35a5b59dd3eb33dcaa61604
yubinm <117456979+yubinm@users.noreply.github.com>
2025-10-23 13:13:32 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "https://yubinm.github.io/yubinm_2d.github.io/",
+  base: "/yubinm_2d.github.io/",
   build: {
     minify: "terser",
   },
