572cb3bbe2f8add4a2134f7b9f3546e8f095aa4d
yubinm <yubinmoon0910@gmail.com>
2025-10-23 13:04:30 -0400

    restarting

--- README.md
+++
@@ -1,1 +0,0 @@
-# yubinm.github.io
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "yubinm_2d",
+  base: "./",
   build: {
     minify: "terser",
   },
