2ae4f2dc6524afacd01ce86865c0c21da44f1d64
yubinm <yubinmoon0910@gmail.com>
2025-10-24 22:53:34 -0400

    Changes to text in interactions

--- src/constants.js
+++ src/constants.js
@@ -2,10 +2,9 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
   pc: `Ah, my workspace! It mostly consists of my laptop, ipad, a waterbottle, and random assortments of pens and paper.
-    I usually work in Python, but I've recently been learning JavaScript and CSS. Here's some of the projects I've worked on so far!`, 
-   //<a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
+    I usually work in Python, but I've recently been learning JavaScript and CSS. Here's some of the projects I've worked on so far! <a href="https://yubinm.github.io/#projects" target="_blank">Project link!</a>`, 
   "cs-degree": `This is my soon-to-be-completed bachelors at Colby College in Statistics and Computer Science with a concentration in Artificial Intelligence dated 2022-2026. I'm proud of what I've done so far!`,
-  tv: `That's my TV. I watch a lot of scientific history youtube videos in my spare time.`,
+  tv: `That's my TV. I watch a lot of scientific history youtube videos in my spare time. Specifically, I've been enjoying <a href="https://www.youtube.com/@TastingHistory" target="_blank">Tasting History with Max Miller</a> these days. `,
   // I've been watching tech youtubers a lot recently like :
   //  <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
   // <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
@@ -15,9 +14,9 @@ export const dialogueData = {
   projects: `This is my desk! On it is my resume. Contact me at yubinmoon0910@gmail.com if you have any interesting job opportunities!`,
   library: `Lots of computer science and statistics homework keys, exams, etc. that I've used over the past few years in my TA and Grader jobs. There's also some books I read for fun; some are even in Korean (I'm fluent in Korean)!`,
   exit: `Leaving? You can close the tab :D`,
-  fridge: `My fridge! `, 
-  sofa: `This is my sofa! When I'm taking a break, I like to lounge about draw. The assets in this house were either edited by me using the Happy Little Adventure spritesheet or drawn from scratch by me entirely!`, 
-  piano: `I've played the piano since I was in 2nd grade, and I still play today! Nowadays, you can find me playing in piano solos, duos, and trios at Colby College. I also played the viola for a bit, though I haven't in a while..`,
+  fridge: `My fridge! There's a variety of different cusines that I enjoy; I'm open to moving around to wherever!`, 
+  sofa: `This is my sofa! When I'm taking a break, I like to lounge about draw. The assets in this house were either edited by me using the Happy Little Adventure 2 spritesheet or drawn from scratch by me through Pixel Studio!`, 
+  piano: `I've played the piano since I was in 2nd grade, and I still play today! Nowadays, you can find me playing in piano solos, duos, and trios at Colby College. I also played the viola for a bit, though I haven't in a while.. I'd be open to opening my viola case someday..`,
   drawers: `I put all my projects and important information here. This portfolio was made with the Kaboom.js library, which is a library for making games in JavaScript.
-  The text is rendered with HTML/CSS. This was made with the help of the <a href="https://youtube.com/@jslegenddev" target="_blank">JSLegendDev tutorial!</a>.`
+  The text is rendered with HTML/CSS. This was made with the help of the <a href="https://youtube.com/@jslegenddev" target="_blank">JSLegendDev tutorial!</a>. For a more comprehensive portfolio, <a href="https://yubinm.github.io" target="_blank">Click here!</a>`
 };
